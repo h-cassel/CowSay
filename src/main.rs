3b46fa08ca5d8463f5d11aef5cae292398940520
Ben C <bwc9876@gmail.com>
2024-03-02 23:06:16 -0500

    Dot env loading for ez stuff

--- src/main.rs
+++ src/main.rs
@@ -82,6 +82,7 @@ const DEFAULT_SOCK_PATH: &str = "/home/pi/printer_data/comms/klippy.sock";
 
 #[tokio::main]
 async fn main() {
+    dotenv::dotenv().ok();
     // Login with a bot token from the environment
     let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
     // Set gateway intents, which decides what events the bot will be notified about
