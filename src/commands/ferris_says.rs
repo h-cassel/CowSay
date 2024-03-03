232e4f85aa450f3cabf93c5482e73c2f03a7bdff
Ben C <bwc9876@gmail.com>
2024-03-03 07:53:09 -0500

    Update

--- src/commands/ferris_says.rs
+++ src/commands/ferris_says.rs
@@ -2,7 +2,6 @@ extern crate ferris_says;
 
 use ferris_says::say;
 use serenity::all::ResolvedValue;
-use std::io::{ Write };
 
 use serenity::builder::CreateCommand;
 use serenity::model::application::ResolvedOption;
@@ -10,16 +9,15 @@ use serenity::model::application::ResolvedOption;
 use crate::state::StateHandle;
 use serenity::all::CommandOptionType;
 use serenity::all::CreateCommandOption;
-use super::slash::SlashCommand;
 
 pub struct FerrisSays;
 
-impl SlashCommand for FerrisSays {
-    fn new(_state_ref: StateHandle) -> Self {
+impl FerrisSays {
+    pub fn new(_state_ref: StateHandle) -> Self {
         Self
     }
 
-    fn run(&self, _options: &[ResolvedOption]) -> String {
+    pub async fn run<'a>(&self, _options: &[ResolvedOption<'a>]) -> String {
 
         let out = if let Some(ResolvedOption { value: ResolvedValue::String(output), .. }) = _options.first() {
             *output
@@ -37,7 +35,7 @@ impl SlashCommand for FerrisSays {
         output2
     }
 
-    fn register() -> CreateCommand {
+    pub fn register() -> CreateCommand {
         CreateCommand::new("ferris-says").description("A visit from Ferris! What does he have to say?").add_option(CreateCommandOption::new(CommandOptionType::String, "dialogue", "What should Ferris say?"))
     }
 }
